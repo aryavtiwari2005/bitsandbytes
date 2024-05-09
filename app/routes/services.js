97c50584640ca62225bf8bb731ede92cb8f46ae6
Aryav Tiwari <aryavtiwari2005@gmail.com>
2024-05-09 20:42:37 +0530

    servicespage

--- app/routes/services.js
+++ app/routes/services.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class ServicesRoute extends Route {}
+export default class ServicesRoute extends Route {
+    @service router;
+
+    beforeModel() {
+        this.router.transitionTo('services')
+    }
+}
