b6d80a9ed86f94cdd46f90757ff0958d0ef01005
Aryav Tiwari <aryavtiwari2005@gmail.com>
2025-01-29 00:07:29 +0530

    changes

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -22,28 +22,38 @@ const navToggle = (toggle) => {
 
 const renderNav = () => {
   const trayIcon = document.querySelector('nav .tray-icon');
-  var trayIconToggle = false;
+  let trayIconToggle = false;
   trayIcon.addEventListener('click', () => {
     trayIconToggle = !trayIconToggle;
     navToggle(trayIconToggle);
   });
 
-  var lastScrollTop = 0;
+  let lastScrollTop = 0;
   $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
+    const scroll = $(window).scrollTop();
     const navbar = document.querySelector('nav');
     const nav = document.querySelector('nav .logo img');
+
+    // Check if the user is scrolling down or up
     if (scroll > lastScrollTop) {
+      // Scrolling down
       navbar.classList.add('animated');
     } else {
+      // Scrolling up
       navbar.classList.remove('animated');
     }
+
+    // Update lastScrollTop
     lastScrollTop = scroll;
 
-    if (scroll > 100) {
+    if (scroll > 110) {
       nav.classList.add('changed');
-    } else if (scroll <= 100) {
+    } else {
       nav.classList.remove('changed');
+    }
+
+    if (scroll < 100) {
+      navbar.classList.remove('animated'); // Remove animation class at the top
     }
   });
 };
